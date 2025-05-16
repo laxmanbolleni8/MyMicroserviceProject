7ced609393d9c467145fe039fd8fb35ac6e8a923
laxmanbolleni8 <laxmanbolleni@gmail.com>
2025-05-16 17:52:40 +0530

    Sample Project

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private static readonly List<string> Products = new List<string> { "Laptop", "Phone" };
+
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        return Ok(Products);
+    }
+
+    [HttpPost]
+    public IActionResult AddProduct([FromBody] string product)
+    {
+        Products.Add(product);
+        return Ok();
+    }
+}
